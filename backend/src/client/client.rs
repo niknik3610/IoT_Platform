3eec73ca23d0b4147e43b1c6e524776cb190b48c
Nik <niklas.harnish@hotmail.com>
2023-10-29 10:55:25 +0100

    added client logic for registration

--- backend/src/client/client.rs
+++ backend/src/client/client.rs
@@ -1,5 +1,10 @@
+pub mod client_registration;
+
+pub const SERVER_IP: &str = "http://[::1]:50051";
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("hello world");
+    let id = client_registration::repeated_register_self().await;
+    println!("{id}");
     Ok(())
 }
