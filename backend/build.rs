0341be9ef2eeb62b61cbcec388319bb42a4e3e2f
Nik <nik@pc-3-163.customer.ask4.lan>
2024-01-11 10:01:32 +0100

    rebased onto main

--- backend/build.rs
+++ backend/build.rs
@@ -1,11 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/iot/types.proto")?;
-    tonic_build::compile_protos("proto/iot/registrationService.proto")?;
-    tonic_build::compile_protos("proto/iot/requestUpdateService.proto")?;
-    tonic_build::compile_protos("proto/iot/deviceControlService.proto")?;
+    tonic_build::compile_protos("../proto/iot/types.proto")?;
+    tonic_build::compile_protos("../proto/iot/registrationService.proto")?;
+    tonic_build::compile_protos("../proto/iot/requestUpdateService.proto")?;
+    tonic_build::compile_protos("../proto/iot/deviceControlService.proto")?;
 
-    tonic_build::compile_protos("proto/frontend/frontend_types.proto")?;
-    tonic_build::compile_protos("proto/frontend/registrationService.proto")?;
-    tonic_build::compile_protos("proto/frontend/frontendDeviceControlService.proto")?;
+    tonic_build::compile_protos("../proto/frontend/frontend_types.proto")?;
+    tonic_build::compile_protos("../proto/frontend/registrationService.proto")?;
+    tonic_build::compile_protos("../proto/frontend/frontendDeviceControlService.proto")?;
     Ok(())
 }
