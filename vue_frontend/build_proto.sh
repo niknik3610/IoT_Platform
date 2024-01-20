73b5c69c1969f3c9ebad07c5c4d7de380176952f
nik <niklas.harnish@hotmail.com>
2024-01-20 12:07:37 +0100

    [chore] removed some comments from build_proto.sh

--- vue_frontend/build_proto.sh
+++ vue_frontend/build_proto.sh
@@ -2,20 +2,3 @@
 mkdir src/generated
 npx pbjs ../proto/frontend/*.proto --target static-module --wrap es6 --out "./src/generated/generated.js" --keep-case
 npx pbts ./src/generated/generated.js --out "./src/generated/generated.d.ts" --no-comments --keep-case
-
-# PROTO_DEST=./src/generated
-# PROTO_SRC=../proto/frontend/
-#
-# mkdir -p ${PROTO_DEST}
-# mkdir -p temp
-#
-# cp ${PROTO_SRC}/*.proto ./temp/
-#
-# cd temp
-# protoc \
-#     --plugin="protoc-gen-ts=../node_modules/.bin/protoc-gen-ts" \
-#     --ts_opt=esModuleInterop=true \
-#     --js_out="../src/generated" \
-#     --ts_out="../src/generated" \
-#      *.proto
-# cd ..
