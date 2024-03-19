d92d7d9ce9f74369615c1a18e1b70e4ef205c79d
nik <niklas.harnish@hotmail.com>
2024-03-19 17:48:27 +0100

    [chore] fixed docs and upgraded version #

--- client_library/src/lib.rs
+++ client_library/src/lib.rs
@@ -4,7 +4,8 @@
 //!
 //! # Example
 //! This is a simple example of how to use this library. It will the server about two buttons
-//! (defined in the config.toml file): "Turn On" and "Turn Off".
+//! (defined in the config.toml file): "Turn On" and "Turn Off". In addition it also shows how a
+//! slider can be used.
 //!
 //! When either of these buttons are pressed, the callback assigned to them will be called, in this
 //! case printing their respective actions.
@@ -15,47 +16,59 @@
 //!
 //! ```
 //! use std::error::Error;
-//! use NOSHP_Client::{client::{ClientHandler, Request, State}, client_config::{ClientConfig, ParsedConfig}};
-//!  
+//! use NOSHP_Client::{
+//!     client::{ClientState, NoshpClient, Request, UserDefinedState},
+//!     client_config::{ClientConfig, ParsedConfig},
+//! };
+//! 
 //! #[derive(Default)]
 //! struct ExampleState {
-//!     text: String,
+//!     pub text: String,
+//!     pub current_brightness_lvl: u32
 //! }
-//! impl State for ExampleState {}
-//!
+//! impl UserDefinedState for ExampleState {}
+//! 
 //! const CONFIG_PATH: &str = "./example_config.toml";
-//!
 //! #[tokio::main]
 //! async fn main() -> Result<(), Box<dyn Error>> {
-//!     let config = ClientConfig::load_config(CONFIG_PATH);
-//!     let config = match config {
-//!         Ok(r) => r,
-//!         Err(e) => {
-//!             eprintln!("Error loading config: {}", e.to_string());
-//!             println!("Loading default config...");
-//!             ParsedConfig::default()
-//!         }
-//!     };
-//!
-//!     let client_handler = ClientHandler::new();
-//!
+//!     let config = ClientConfig::load_config(CONFIG_PATH).unwrap();
+//! 
+//!     let client_handler = NoshpClient::new();
 //!     client_handler
-//!     .set_state(ExampleState { text: String::from("hello world") })
-//!     .add_callback("Turn On", Box::new(turn_on_led))
-//!     .add_callback("Turn Off", Box::new(turn_off_led))
-//!     .run(config)
-//!     .await.unwrap();
-//!
+//!         .set_state(ExampleState {
+//!             text: String::from("hello world"),
+//!             current_brightness_lvl: 50,
+//!         })
+//!         .add_callback("Turn On", Box::new(turn_on_led))
+//!         .add_callback("Turn Off", Box::new(turn_off_led))
+//!         .add_callback("Brightness", Box::new(handle_brightness))
+//!         .run(config)
+//!         .await
+//!         .unwrap();
+//! 
 //!     return Ok(());
 //! }
-//!
-//! fn turn_on_led(state: &mut ExampleState, req: Request) {
-//!     println!("turned on led");
+//! 
+//! fn turn_on_led(state: &mut ClientState<ExampleState>, _req: Request) {
+//!     state.update_capability_availabillity("Turn On", false).unwrap();
+//!     state.update_capability_availabillity("Turn Off", true).unwrap();
+//!     state.user_state.text = String::from("Turned On");
+//! 
+//!     println!("State: {}", state.user_state.text);
 //! }
-//! fn turn_off_led(state: &mut ExampleState, req: Request) {
-//!     println!("turned off led");
+//! 
+//! fn turn_off_led(state: &mut ClientState<ExampleState>, _req: Request) {
+//!     state.update_capability_availabillity("Turn On", true).unwrap();
+//!     state.update_capability_availabillity("Turn Off", false).unwrap();
+//!     state.user_state.text = String::from("Turned Off");
+//!     println!("State: {}", state.user_state.text);
 //! }
-//! ```
+//! 
+//! fn handle_brightness(state: &mut ClientState<ExampleState>, req: Request) {
+//!     let new_brightness = req.value.unwrap();
+//!     state.user_state.current_brightness_lvl = new_brightness as u32;
+//!     println!("New brightness set: {}", state.user_state.current_brightness_lvl);
+//! }
 //!
 
 pub mod client;
