94409684ff0eae9085b0a6760a481de0c0d44d62
nik <niklas.harnish@hotmail.com>
2024-03-22 11:49:04 +0100

    [fix] add a compiler flag to protobuf compiler

--- backend/build.rs
+++ backend/build.rs
@@ -1,10 +1,18 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../proto/iot/types.proto")?;
-    tonic_build::compile_protos("../proto/iot/registrationService.proto")?;
-    tonic_build::compile_protos("../proto/iot/requestUpdateService.proto")?;
-    tonic_build::compile_protos("../proto/iot/deviceControlService.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(
+            &[
+                "../proto/iot/registrationService.proto",
+                "../proto/iot/requestUpdateService.proto",
+                "../proto/iot/types.proto",
+                "../proto/iot/deviceControlService.proto",
+            ],
+            &["../proto/iot"],
+        )?;
 
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
         .compile(
             &[
